a7cc37433d95d14e64887b92e792e36f539cca76
beatzball <38116726+beatzball@users.noreply.github.com>
2026-09-23 23:52:25 -0500

    Remove the file that had to turn CI red

--- internal/protocol/badly_formatted_on_purpose.go
+++
@@ -1,7 +0,0 @@
-package protocol
-
-// This file exists for one commit only: it proves CI turns red on an
-// unformatted file. The next commit deletes it.
-func   badlyFormatted( ) int {
-		return 1
-}
